c1a7eda2b8e8d125be035e68ee6072af0eb8ab8a
Azad <AzadAmirir88@gmail.com>
2025-01-08 23:58:57 +0330

    shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="fon-bold text-blue-500">Welcome to the course!</h1>
-      <h1>heading</h1>
+      <h1 className="text-xl text-blue-500">Welcome to the course!</h1>
+      <h1 className="text-4xl">heading</h1>
+      <Button className="text-xl" variant={"destructive"}>Click me</Button>
     </div>
   );
 }
